4ab3833eeafbd39a4a600237975c8a32e2a94f29
Ashutosh7634 <>
2022-07-17 12:45:12 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("ashu")
+var a= 45;
+let b="harry";
+var d=null
+var c= undefined
+const author="Harry"
+//author=5  throws an error because an constant can't be changed 
+{
+  //var declare variables in globally while let declares variables locally
+  let b= "This"
+  console.log(b)
+}
+console.log(b) 
